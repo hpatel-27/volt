97629039d294ca20a29651896cf6d1536849eb88
Harsh Patel <harshpxv@gmail.com>
2026-03-31 00:43:44 -0400

    feat: set a protected route
    requireAuth should redirect a user to sign in if they are not authenticated.

--- backend-volt/src/server.ts
+++ backend-volt/src/server.ts
@@ -1,11 +1,29 @@
 import "dotenv/config";
-
 import express from "express";
+import {
+  clerkMiddleware,
+  clerkClient,
+  requireAuth,
+  getAuth,
+} from "@clerk/express";
+
 const app = express();
 const port = process.env.SERVER_PORT;
 
-app.get("/", (req, res) => {
-  res.send("Hello World!");
+app.use(clerkMiddleware());
+
+// Use requireAuth() to protect this route
+// If user isn't authenticated, requireAuth() will redirect back to the homepage
+app.get("/api/protected", requireAuth(), async (req, res) => {
+  // Use `getAuth()` to get the user's `userId`
+  const { userId } = getAuth(req);
+  if (!userId) {
+    return res.status(401).json({ error: "Unauthorized" });
+  }
+  // Use Clerk's JS Backend SDK to get the user's User object
+  const user = await clerkClient.users.getUser(userId);
+
+  return res.json({ user });
 });
 
 app.listen(port, () => {
