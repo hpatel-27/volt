4043a6cf4d81b892a5090ab15853490be7cfb7a8
Harsh Patel <harshpxv@gmail.com>
2026-03-31 17:47:09 -0400

    feat: initial setup for frontend fetch - Get token from Clerk and send with request - Abstract auth token injection into reusable hook

--- frontend-volt/src/pages/Landing.tsx
+++ frontend-volt/src/pages/Landing.tsx
@@ -4,9 +4,26 @@ import {
   SignInButton,
   SignUpButton,
   UserButton,
+  useAuth,
 } from "@clerk/clerk-react";
 
 const Home = () => {
+  const { getToken } = useAuth();
+  const token = async () => await getToken();
+
+  const user = fetch("http://localhost:8080/api/v1/example/protected", {
+    headers: {
+      "Content-Type": "application/json",
+      Authorization: `Bearer ${token}`,
+    },
+  })
+    .then((res) => res.json())
+    .catch((err) => {
+      console.error("Error fetching protected data:", err);
+      return null;
+    });
+  console.log("Fetched user data:", user);
+
   return (
     <>
       <header>
