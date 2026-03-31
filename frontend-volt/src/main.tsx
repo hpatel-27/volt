9648abf7260d2d183366c2cc50415a84dc1ded6e
Harsh Patel <harshpxv@gmail.com>
2026-03-31 00:41:12 -0400

    feat: configure clerk with React
    - Install the clerk dependency
    - Set the ClerkProvider with the API Key
    - Show sign in and sign up when not signed in and show the user button when signed in

--- frontend-volt/src/main.tsx
+++ frontend-volt/src/main.tsx
@@ -1,10 +1,14 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App.tsx";
+import { ClerkProvider } from "@clerk/react";
+const CLERK_PUBLISHABLE_KEY = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY;
 
-createRoot(document.getElementById('root')!).render(
+createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <App />
+    <ClerkProvider publishableKey={CLERK_PUBLISHABLE_KEY}>
+      <App />
+    </ClerkProvider>
   </StrictMode>,
-)
+);
