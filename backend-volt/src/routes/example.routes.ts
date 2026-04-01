5fa31df6d0556c057feb44f051026f0fbbbde297
Harsh Patel <harshpxv@gmail.com>
2026-03-31 22:10:35 -0400

    fix: update import path - On the update to the auth middleware, the path was changed resulting in a change here

--- backend-volt/src/routes/example.routes.ts
+++ backend-volt/src/routes/example.routes.ts
@@ -1,6 +1,6 @@
 import express from "express";
 import { clerkClient, getAuth } from "@clerk/express";
-import { hasPermission } from "../middleware/auth.js";
+import { hasPermission } from "../middleware/auth.middleware.js";
 
 const router = express.Router();
 
