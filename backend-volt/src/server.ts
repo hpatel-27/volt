b2d5c284f3fdb72440988ac65c80494766342478
Harsh Patel <harshpxv@gmail.com>
2026-04-02 13:10:08 -0400

    feat: rate limit config - Comment to highlight the limiter config is for rate limiting the API

--- backend-volt/src/server.ts
+++ backend-volt/src/server.ts
@@ -25,6 +25,7 @@ app.use(
   }),
 );
 
+// Rate limiting configuration
 const limiter = rateLimit({
   windowMs: 15 * 60 * 1000, // 15 minutes
   limit: 100, // Limit each IP to 100 requests per `window` (here, per 15 minutes).
