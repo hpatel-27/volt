bcc28aa4edf512063246bac72629129740dbce46
Harsh Patel <harshpxv@gmail.com>
2026-04-02 10:14:34 -0400

    feat: rate limit API - Install express-rate-limit - Set a limit of 100 requests over a 15 minute window

--- backend-volt/src/server.ts
+++ backend-volt/src/server.ts
@@ -2,6 +2,7 @@ import "dotenv/config";
 import express from "express";
 import { clerkMiddleware } from "@clerk/express";
 import cors from "cors";
+import { rateLimit } from "express-rate-limit";
 import routeIndex from "./routes/index.js";
 
 const API_BASE = process.env.API_BASE || "/api/v1";
@@ -23,6 +24,16 @@ app.use(
     allowedHeaders: ["Content-Type", "Authorization", "X-Requested-With"],
   }),
 );
+
+const limiter = rateLimit({
+  windowMs: 15 * 60 * 1000, // 15 minutes
+  limit: 100, // Limit each IP to 100 requests per `window` (here, per 15 minutes).
+  standardHeaders: "draft-8", // draft-6: `RateLimit-*` headers; draft-7 & draft-8: combined `RateLimit` header
+  legacyHeaders: false, // Disable the `X-RateLimit-*` headers.
+  ipv6Subnet: 56, // Set to 60 or 64 to be less aggressive, or 52 or 48 to be more aggressive
+  // store: ... , // Redis, Memcached, etc. See below.
+});
+app.use(limiter);
 
 app.use(clerkMiddleware());
 
