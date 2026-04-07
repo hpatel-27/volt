6523efcde816fd2b54c61c4f693a28c96f1f6dea
Harsh Patel <harshpxv@gmail.com>
2026-04-06 22:32:06 -0400

    test: setting up files for testing

---
+++ backend-volt/test/unit/services/meal.test.ts
@@ -0,0 +1,9 @@
+import { expect, test } from "vitest";
+import * as mealService from "../../../src/services/meal.service.js";
+
+test("Unit testing getAllMeals", () => {
+  test("Invalid meal - Log not found", () => {});
+
+  test("No meals in list", () => {});
+  test("Multiple meals in list", () => {});
+});
