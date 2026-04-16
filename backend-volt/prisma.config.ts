fb931bbb66d29e79922758949e00f2300cac0573
Harsh Patel <harshpxv@gmail.com>
2026-04-16 16:58:20 -0400

    test: setup test database - Use the test database url when the node_env is set to "test"

--- backend-volt/prisma.config.ts
+++ backend-volt/prisma.config.ts
@@ -5,6 +5,7 @@ export default defineConfig({
   schema: "./prisma/schema.prisma",
   datasource: {
     url: env(
+      // "TEST_DATABASE_URL",
       process.env.NODE_ENV === "production"
         ? "DATABASE_URL"
         : "DEV_DATABASE_URL",
--- backend-volt/src/db.ts
+++ backend-volt/src/db.ts
@@ -1,14 +1,23 @@
+import "dotenv/config";
 import { PrismaClient } from "./generated/prisma/client.js";
 import { PrismaPg } from "@prisma/adapter-pg";
 
-if (!process.env.DATABASE_URL || !process.env.DEV_DATABASE_URL) {
-  throw new Error("DATABASE_URL is not defined");
+if (
+  !process.env.DATABASE_URL ||
+  !process.env.DEV_DATABASE_URL ||
+  !process.env.TEST_DATABASE_URL
+) {
+  throw new Error("DATABASE_URLs are not defined");
 }
 
-const DATABASE_URL =
-  process.env.NODE_ENV === "production"
-    ? process.env.DATABASE_URL
-    : process.env.DEV_DATABASE_URL;
+let DATABASE_URL;
+if (process.env.NODE_ENV === "production") {
+  DATABASE_URL = process.env.DATABASE_URL;
+} else if (process.env.NODE_ENV === "development") {
+  DATABASE_URL = process.env.DEV_DATABASE_URL;
+} else if (process.env.NODE_ENV === "test") {
+  DATABASE_URL = process.env.TEST_DATABASE_URL;
+}
 
 const adapter = new PrismaPg({
   connectionString: DATABASE_URL,
