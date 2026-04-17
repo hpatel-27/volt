22072883e6b272251447c2a016c66398e79cc194
Harsh Patel <harshpxv@gmail.com>
2026-04-17 17:57:53 -0400

    fix: merge route params - A parent route's params are not visible in the child unless the mergeParams option is enabled.

--- backend-volt/src/routes/meal.routes.ts
+++ backend-volt/src/routes/meal.routes.ts
@@ -1,5 +1,5 @@
 import express from "express";
-const router = express.Router();
+const router = express.Router({ mergeParams: true });
 
 import * as mealController from "../controllers/meal.controller.js";
 // Meal Routes
