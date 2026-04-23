f4fc3bc73bc937b8cc5a6fd5f3e3e4b78372ce89
Harsh Patel <harshpxv@gmail.com>
2026-04-22 20:10:00 -0400

    feat: set pagination details on Request

--- backend-volt/@types/express/index.d.ts
+++ backend-volt/@types/express/index.d.ts
@@ -1,10 +1,12 @@
 import type { User } from "../../src/generated/prisma/client.js";
+import type { Pagination } from "../../src/types/pagination.ts";
 
 declare global {
   namespace Express {
     interface Request {
       user?: User;
       id?: string;
+      pagination?: Pagination;
     }
   }
 }
