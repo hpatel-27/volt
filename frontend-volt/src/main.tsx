1ae97e436fafcb635bb588bc3b41c0f1d5c3e92b
Harsh Patel <harshpxv@gmail.com>
2026-05-05 12:43:03 -0400

    feat: wrap app with queryClientProvider & devTools

--- frontend-volt/src/main.tsx
+++ frontend-volt/src/main.tsx
@@ -3,11 +3,17 @@ import { createRoot } from "react-dom/client";
 import "./index.css";
 import { App } from "./App";
 import { BrowserRouter } from "react-router";
+import { QueryClientProvider } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+import { queryClient } from "./lib/queryClient";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <BrowserRouter>
-      <App />
+      <QueryClientProvider client={queryClient}>
+        <App />
+        <ReactQueryDevtools initialIsOpen={false} />
+      </QueryClientProvider>
     </BrowserRouter>
   </StrictMode>,
 );
