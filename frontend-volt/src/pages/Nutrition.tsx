f88bd19b3c03428fb1f7dc6886de498bbc0fa587
Harsh Patel <harshpxv@gmail.com>
2026-05-19 12:13:49 -0400

    feat: display dynamic nutrition log data

--- frontend-volt/src/pages/Nutrition.tsx
+++ frontend-volt/src/pages/Nutrition.tsx
@@ -1,17 +1,11 @@
 import { Link } from "react-router";
 import { Button } from "../components/ui/Button";
 import { Card } from "../components/ui/Card";
-import { useNutritionLogs } from "@/api/nutrition";
+import { useNutritionLogs, useNutritionRange } from "@/api/nutrition";
 import { useState } from "react";
 import { formatVerboseDate, todayLocalIso } from "@/lib/date";
-import { ChevronLeft, ChevronRight, Plus } from "lucide-react";
-
-const today = { id: "today", date: "Today · Apr 26", kcal: 1558, target: 2500 };
-const past = [
-  { id: "2026-04-25", date: "Yesterday", kcal: 2310 },
-  { id: "2026-04-24", date: "Apr 24", kcal: 2204 },
-  { id: "2026-04-23", date: "Apr 23", kcal: 2502 },
-];
+import { ChevronLeft, ChevronRight, Plus, Utensils } from "lucide-react";
+import { Spinner } from "@/components/ui/Spinner";
 
 export default function Nutrition() {
   const LIMIT = 10;
@@ -24,13 +18,20 @@ export default function Nutrition() {
     pageQuery.data?.total !== 0
       ? Math.ceil(pageQuery.data?.total / pageQuery.data?.limit)
       : 1;
-
+  const todayIso = todayLocalIso();
+  // Workout to get summarized nutrition data for a dynamic "today"
+  // Currently i haven't gotten around to updating the API to return
+  // a summary of the meals for a day instead of all the actual meals
+  // but that will make it easy to display the calories info for the
+  // index nutrition page
+  const todayQuery = useNutritionRange({ from: todayIso, to: todayIso });
+  console.log(todayQuery.data);
   return (
     <div className="space-y-4 pt-4">
       <header className="flex items-center justify-between">
         <div>
           <div className="text-caption">
-            Today · {formatVerboseDate(todayLocalIso())}
+            Today · {formatVerboseDate(todayIso)}
           </div>
           <h1 className="font-display text-2xl font-bold">Nutrition</h1>
         </div>
@@ -38,60 +39,87 @@ export default function Nutrition() {
           variant="sky"
           size="sm"
           className="cursor-pointer active:bg-sky-700"
+          onClick={() => console.log("clicked main meal log button")}
         >
-          <Plus className="w-5 h-5" /> <span>Meal</span>
+          <Plus className="w-4 h-4 shrink-0 translate-y-px" />
+          <span className="">Meal</span>
         </Button>
       </header>
 
-      <Link to={`/nutrition/${today.id}`}>
+      <Link to={`/nutrition/${todayIso}`}>
         <Card interactive accent="sky" className="border-sky-500/20">
           <div className="text-caption mb-2">Today</div>
           <div className="flex items-baseline gap-2">
             <span className="text-stat font-display text-sky-500">
-              {today.kcal}
+              {todayQuery.data?.logs.length !== 0
+                ? todayQuery.data?.logs[0]?.id
+                : 0}
             </span>
-            <span className="text-bone-500 text-xs">/ {today.target} kcal</span>
+            <span className="text-bone-500 text-xs">/ {2500} kcal</span>
           </div>
         </Card>
       </Link>
 
-      {/* <div className="text-caption">Past 6 days</div> */}
-      <div className="space-y-2 pt-6">
-        <span className="text-caption">Other Entries</span>
-        {past.map((d) => (
-          <Link key={d.id} to={`/nutrition/${d.id}`}>
-            <Card
-              interactive
-              className="flex items-center justify-between p-4 mt-2"
+      {pageQuery.isLoading ? (
+        <Spinner />
+      ) : (pageQuery.data?.nutritionLogs.length ?? 0) > 0 ? (
+        <div className="space-y-2 pt-6">
+          <span className="text-caption">Other Entries</span>
+          {pageQuery.data?.nutritionLogs.map((day) => (
+            <Link key={day.id} to={`/nutrition/${day.id}`}>
+              <Card
+                interactive
+                className="flex items-center justify-between p-4 mt-2"
+              >
+                <div className="text-sm font-semibold">
+                  {formatVerboseDate(day.date)}
+                </div>
+                <div className="font-mono text-sm">{0} kcal</div>
+              </Card>
+            </Link>
+          ))}
+          <div className="flex pt-6 justify-center gap-2">
+            <Button
+              size="sm"
+              variant="sky"
+              disabled={page === 1}
+              onClick={() => setPage((p) => p - 1)}
+              className="hover:text-ink-950 hover:font-semibold active:bg-sky-700 cursor-pointer transition"
             >
-              <div className="text-sm font-semibold">{d.date}</div>
-              <div className="font-mono text-sm">{d.kcal} kcal</div>
-            </Card>
-          </Link>
-        ))}
-        <div className="flex pt-6 justify-center gap-2">
+              <ChevronLeft />
+              Prev
+            </Button>
+            <Button
+              size="sm"
+              variant="sky"
+              disabled={page === totalPages}
+              onClick={() => setPage((p) => p + 1)}
+              className="hover:text-ink-950 hover:font-semibold active:bg-sky-700 cursor-pointer transition"
+            >
+              Next
+              <ChevronRight />
+            </Button>
+          </div>
+        </div>
+      ) : (
+        <div className="flex flex-col items-center text-center py-16 gap-2">
+          <Utensils className="w-12 h-12 text-bone-500" />
+          <p className="font-display text-lg font-semibold text-bone-500">
+            Let's get started.
+          </p>
+          <p className="text-caption max-w-xs">
+            Log your first meal to start tracking your nutrition.
+          </p>
           <Button
-            size="sm"
             variant="sky"
-            disabled={page === 1}
-            onClick={() => setPage((p) => p - 1)}
-            className="hover:text-ink-950 hover:font-semibold active:bg-sky-700 cursor-pointer transition"
-          >
-            <ChevronLeft />
-            Prev
-          </Button>
-          <Button
             size="sm"
-            variant="sky"
-            disabled={page === totalPages}
-            onClick={() => setPage((p) => p + 1)}
-            className="hover:text-ink-950 hover:font-semibold active:bg-sky-700 cursor-pointer transition"
+            className="cursor-pointer active:bg-sky-700"
+            onClick={() => console.log("clicked empty state button")}
           >
-            Next
-            <ChevronRight />
+            <span>Log a meal</span>
           </Button>
         </div>
-      </div>
+      )}
     </div>
   );
 }
