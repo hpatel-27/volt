7f1163acd79e3b5588ef75bf3eb7ab82f68149a1
Harsh Patel <harshpxv@gmail.com>
2026-05-25 12:43:01 -0400

    refactor: export shared constants

---
+++ frontend-volt/src/types/shared.ts
@@ -0,0 +1,13 @@
+// Shared constants
+
+// Limit for pagination
+export const LIMIT = 10;
+
+// Nutrition Goals
+export const CALORIE_GOAL = 2100;
+export const GOALS = {
+  calories: 2100,
+  protein: 140, // 560 calories
+  carbs: 210, // 840 calories
+  fat: 50, // 450 calories
+};
