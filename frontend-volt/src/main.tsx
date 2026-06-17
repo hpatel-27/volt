75fd6a1c8fd9cbd5da119ad744d34fb732efb9fc
Harsh Patel <harshpxv@gmail.com>
2026-06-16 21:45:31 -0400

    chore: vercel config (#86)
    - Setup the Vercel analytics and speed insights
    - Since BrowserRouter uses virtual client side paths, they are all based on index.html, while Vercel is looking for a real built file that matches that path

--- frontend-volt/src/main.tsx
+++ frontend-volt/src/main.tsx
@@ -6,6 +6,8 @@ import { BrowserRouter } from "react-router";
 import { QueryClientProvider } from "@tanstack/react-query";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 import { queryClient } from "./lib/queryClient";
+import { Analytics } from "@vercel/analytics/react";
+import { SpeedInsights } from "@vercel/speed-insights/react";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
@@ -13,6 +15,8 @@ createRoot(document.getElementById("root")!).render(
       <QueryClientProvider client={queryClient}>
         <App />
         <ReactQueryDevtools initialIsOpen={false} />
+        <Analytics />
+        <SpeedInsights />
       </QueryClientProvider>
     </BrowserRouter>
   </StrictMode>,
